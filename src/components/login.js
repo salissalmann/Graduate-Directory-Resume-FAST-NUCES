bf8d276c843b990d52ed689085f65ffd8b1a4742
mariasaeedahmed <87717273+mariasaeedahmed@users.noreply.github.com>
2023-04-02 02:14:46 +0500

    Add files via upload

---
+++ src/components/login.js
@@ -0,0 +1,26 @@
+import React from "react";
+class Login extends React.Component{
+    render(){
+        return(
+            <div>
+                <div class="login-container">
+    <section class="login" id="login">
+      <header>
+        <h2>Graduating Directory Resume</h2>
+        <h4>Login</h4>
+      </header>
+      <form class="login-form" action="#" method="post">
+        <input type="text" class="login-input" placeholder="User" required autofocus/>
+        <input type="password" class="login-input" placeholder="Password" required/>
+        <div class="submit-container">
+          <button type="submit" class="login-button">SIGN IN</button>
+        </div>
+      </form>
+    </section>
+    
+  </div>
+            </div>
+        );
+    }
+}
+export default Login;
